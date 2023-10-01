9197620544505b9e28c9856923192924a3c03f72
kaenovsky <martinkaen@gmail.com>
2023-10-01 16:44:50 -0300

    Add comments to views.py

--- zhongwen/zhongwen/urls.py
+++ zhongwen/zhongwen/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-# from zhongwen.views import static_page
 from . import views
 
 urlpatterns = [
--- zhongwen/zhongwen/views.py
+++ zhongwen/zhongwen/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import Http404
 
+# Define a view function 'static_page' that takes a 'request' and a 'pagename' as parameters.
 def static_page(request, pagename):
     try:
+        # Try to render an HTML template with the given 'pagename'.
+        # Create a 'context' dictionary to pass data to the template.
         context = {
             'pagename': pagename,
+            # Add any other data to context here.
         }
+        # Use the 'render' function to render the template with the provided 'context'.
         return render(request, f"{pagename}.html", context)
     except:
+        # If an exception occurs during rendering, raise a 404 error.
         raise Http404("Page not found")
