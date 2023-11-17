43f41737198d16179252a192796642ca5fecce26
kaenovsky <martinkaen@gmail.com>
2023-11-17 01:43:53 -0300

    Improve style, add menu item, add wordcount

--- zhongwen/apps/words/views.py
+++ zhongwen/apps/words/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from apps.words.models import Word
+
+# convert querySet to list
+words_list = list(Word.objects.all())
 
 class WordsTemplateView(LoginRequiredMixin, TemplateView):
     template_name = "words.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['es'] = words_list[0].es
+        context['en'] = words_list[0].en
+        context['zw'] = words_list[0].zw
+        context['pinyin'] = words_list[0].pinyin
         return context
--- zhongwen/zhongwen/views.py
+++ zhongwen/zhongwen/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import Http404
+from apps.words.models import Word
+
+# convert querySet to list
+words_list = list(Word.objects.all())
+
+# get wordcount with len()
+total_wordcount = len(words_list)
 
 # Define a view function 'static_page' that takes a 'request' and a 'pagename' as parameters.
 def static_page(request, pagename):
@@ -9,6 +16,7 @@ def static_page(request, pagename):
         context = {
             'pagename': pagename,
             # Add any other data to context here.
+            'wordcount': total_wordcount
         }
         # Use the 'render' function to render the template with the provided 'context'.
         return render(request, f"{pagename}.html", context)
