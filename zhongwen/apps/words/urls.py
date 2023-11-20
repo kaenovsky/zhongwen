a26d8ca05ccb245d70bf16fe0c2d933df052110b
kaenovsky <martinkaen@gmail.com>
2023-11-20 19:21:20 -0300

    Finish CRUD words

--- zhongwen/apps/words/urls.py
+++ zhongwen/apps/words/urls.py
@@ -1,11 +1,21 @@
 from django.urls import path
 
-from apps.words.views import WordsTemplateView, WordTemplateView, WordFormView, WordSuccess #, WordUpdateFormView
+from apps.words.views import (
+    WordsTemplateView,
+    WordTemplateView,
+    AddWordTemplateView,
+    EditWordUpdateView,
+    RemoveWordDeleteView,
+    WordSuccess,
+    WordSuccessDel,
+)
 
 urlpatterns = [
     path("", WordsTemplateView.as_view(), name="words"),
     path("<int:id>", WordTemplateView.as_view(), name="word"),
-    path("create", WordFormView.as_view(), name="word_create"),
+    path("create", AddWordTemplateView.as_view(), name="word_create"),
+    path("<int:pk>/update", EditWordUpdateView.as_view(), name="word_update"),
+    path("<int:pk>/delete", RemoveWordDeleteView.as_view(), name="word_delete"),
     path("word_success", WordSuccess.as_view(), name="word_success"),
-    # path("<int:id>/update", WordUpdateFormView.as_view(), name="word_update"),
+    path("word_success_del", WordSuccessDel.as_view(), name="word_success_del"),
 ]
--- zhongwen/apps/words/views.py
+++ zhongwen/apps/words/views.py
@@ -1,8 +1,7 @@
 import os
-from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404, render, redirect
+from django.urls import reverse
 from django.views.generic import TemplateView
-from django.views.generic.edit import CreateView
 from django.views.generic.edit import UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from apps.words.models import Word
@@ -22,21 +21,55 @@ class WordTemplateView(LoginRequiredMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        word = Word.objects.get(id=kwargs['id'])
-        if not word:
-            pass
-        context["word"] = word 
+        id = kwargs.get("id")
+        context["word"] = get_object_or_404(Word, id=id) 
         return context
-    
-class WordFormView(LoginRequiredMixin, CreateView):
-    form_class = WordForm
-    template_name = 'word_create.html'
-    success_url = reverse_lazy('word_success')
 
-    def form_valid(self, form):
-        form.save()
-        return super().form_valid(form)
+class AddWordTemplateView(LoginRequiredMixin, TemplateView):
+    template_name = "word_create.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = WordForm() 
+        return context
+
+    def post(self, request):
+        form = WordForm(request.POST)
+
+        if form.is_valid():
+            word = form.save(commit=False)
+            word.save()
+            return redirect("word_success")
+        
+        return self.render_to_response({"form": form})
+
+class EditWordUpdateView(LoginRequiredMixin, UpdateView):
+    model = Word
+    form_class = WordForm
+    template_name = "word_update.html"
+
+    def get_success_url(self):
+        return reverse("word_success")
+
+    def get(self, request, *args, **kwargs):
+        word = self.get_object()
+        return super().get(request, *args, **kwargs)
+
+class RemoveWordDeleteView(LoginRequiredMixin, DeleteView):
+    model = Word
+    template_name = "word_delete.html"
+
+    def get_success_url(self):
+        return reverse("word_success_del")
+
+    def get(self, request, *args, **kwargs):
+        word = self.get_object()
+        return super().get(request, *args, **kwargs)
+
+# Templates create/update and word removed
 
 class WordSuccess(LoginRequiredMixin, TemplateView):
     template_name = "word_success.html"
 
+class WordSuccessDel(LoginRequiredMixin, TemplateView):
+    template_name = "word_success_del.html"
