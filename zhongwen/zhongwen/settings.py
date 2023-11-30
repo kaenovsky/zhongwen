be8dc5b9418a09ae45a2e1257b9a6248ad2c1dcb
kaenovsky <martinkaen@gmail.com>
2023-11-30 20:25:58 -0300

    set debug false

--- zhongwen/zhongwen/settings.py
+++ zhongwen/zhongwen/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0ae738ab(m(cjl&d3y4em&dzs7rfvsdma=vo8_y^j(ssq02f73'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
